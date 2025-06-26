18c87f82fad3cf421a7527a94e9b32999e01dca2
wdoliv <131808851+wdoliv@users.noreply.github.com>
2025-06-26 03:35:46 -0300

    reward.go

--- reward.go
+++ reward.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/big"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -13,9 +15,26 @@ import (
 func getBlockRewards(ctx context.Context, chainId uint64, client *ethclient.Client, block *types.Block, blockReceipts []*types.Receipt) (map[common.Address]*big.Int, map[common.Hash]*big.Int) {
 	var result []map[string]any
 	blockHeader := block.Header()
-	retryUntilSuccessOrContextDone(ctx, func(ctx context.Context) error {
-		return client.Client().CallContext(ctx, &result, "trace_block", fmt.Sprintf("0x%x", blockHeader.Number.Uint64()))
+	err := retryUntilSuccessOrContextDone(ctx, func(ctx context.Context) error {
+		err := client.Client().CallContext(ctx, &result, "trace_block", fmt.Sprintf("0x%x", blockHeader.Number.Uint64()))
+		if err != nil {
+			// Se o método trace_block não existe, apenas loga e retorna nil para não falhar
+			if strings.Contains(err.Error(), "trace_block") && strings.Contains(err.Error(), "does not exist") {
+				log.Printf("trace_block não disponível no nó RPC, ignorando recompensas por trace_block")
+				return nil
+			}
+			return err
+		}
+		return nil
 	}, "trace_block")
+
+	if err != nil {
+		log.Printf("Erro na chamada trace_block: %v", err)
+		// Decide aqui se quer continuar (retornando mapas vazios) ou abortar com panic/return nil
+		// Vou optar por continuar com mapas vazios:
+		return make(map[common.Address]*big.Int), make(map[common.Hash]*big.Int)
+	}
+
 	rewardsByMiner := make(map[common.Address]*big.Int)
 	rewardsByUncleBlock := make(map[common.Hash]*big.Int)
 	uncleHeaders := make(map[*types.Header]struct{})
